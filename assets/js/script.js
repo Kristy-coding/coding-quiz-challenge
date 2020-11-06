14eb67858180ced0a842e1634e14142b579587b3
Kristy Zurmuhlen <kristyzurmuhlen@gmail.com>
2020-11-06 13:37:59 -0500

    started endQuiz funciton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,38 @@
 
+// GLOBAL VARIABLES and OBJECTS//
 
-var startButtonEl = document.getElementById('start-btn');
+//mapping targeted HTML elements to the DOM//
+
+var startButtonEl = document.getElementById("start-btn");
 
 var startChallengeContainerEl = document.getElementById("start-challenge-container");
 
 var quizContainerEl = document.getElementById("quiz-container");
 
-var questionTitle = document.querySelector("#question")
-var button1 = document.querySelector("#answer-button1")
-var button2 = document.querySelector("#answer-button2")
-var button3 = document.querySelector("#answer-button3")
-var button4 = document.querySelector("#answer-button4") 
+var finalScoreFormEl =document.getElementById("final-score-form");
 
-var index = 0
+var questionTitle = document.getElementById("question");
 
-var score = 0
+var button1 = document.getElementById("answer-button1");
+
+var button2 = document.getElementById("answer-button2");
+
+var button3 = document.getElementById("answer-button3");
+
+var button4 = document.getElementById("answer-button4"); 
+
+var index = 0;
+
+var score = 0;
 
 var questionObjArray = [
     { //questionObj [0]
         title: "Arrays in JavaScript can be used to store___.",
         choices: [
-            "numbers and strings",
-            "other arrays",
-            "booleans",
-            "all of the above"
+            "numbers and strings", //[0]
+            "other arrays",        //[1]
+            "booleans",            //[2]
+            "all of the above"     //[3]
         ],
         answer: "all of the above"
     
@@ -32,49 +41,46 @@ var questionObjArray = [
     {// questionObj [1]
         title: "questions 2.",
         choices:[
-            "option 1",
-            "option 2",
-            "option 3",
-            "option 4",
+            "option 1", //[0]
+            "option 2", //[1]
+            "option 3", //[2]
+            "option 4", //[3]
         ],
         answer: "option 3"
     },
      {// questionObj [2]
         title: "questions 3.",
         choices:[
-            "option 1",
-            "option 2",
-            "option 3",
-            "option 4",
+            "option 1", //[0]
+            "option 2", //[1]
+            "option 3", //[2]
+            "option 4", //[3]
         ],
         answer: "option 2"
     },
      {// questionObj [3]
         title: "questions 4.",
         choices:[
-            "option 1",
-            "option 2",
-            "option 3",
-            "option 4",
+            "option 1", //[0]
+            "option 2", //[1]
+            "option 3", //[2]
+            "option 4", //[3]
         ],
         answer: "option 1"
     },
-]
-
-
-
+];
 
 // when we load the page we see the start-challenge-container and header
 // the rest of the content is hiden 
-//we listen for a button click on start-button
-//when the start button is clicked the start button event listener triggers startQuiz()
+// we listen for a button click on start-button
+// when the start button is clicked the start button event listener triggers startQuiz()
 
 var startQuiz = function(){
-// first we hide the start-challenge-container
-startChallengeContainerEl.classList.add("hide");
-// then the timer starts 
 
-// we then want to call displayQuiz() to present the first quesiton 
+    startChallengeContainerEl.classList.add("hide");
+    // then the timer starts 
+
+    // we then want to call displayQuiz() to present the first quesiton 
 
     displayQuiz();
 };
@@ -84,36 +90,55 @@ var displayQuiz = function(){
 
     quizContainerEl.classList.remove("hide"); 
     
-        questionTitle.innerText = questionObjArray[index].title 
+    // to display the quiz and questions at each index target the text content of the HTML DOM elements and set their value to the desired properties/strings defined in the questionObjArray at that [index]
 
-        button1.innerText = questionObjArray[index].choices[0];
+    questionTitle.textContent = questionObjArray[index].title 
 
-        button2.innerText = questionObjArray[index].choices[1];
+    button1.textContent = questionObjArray[index].choices[0];
 
-        button3.innerText = questionObjArray[index].choices[2];
+    button2.textContent = questionObjArray[index].choices[1];
 
-        button4.innerText = questionObjArray[index].choices[3]; 
+    button3.textContent = questionObjArray[index].choices[2];
+
+    button4.textContent = questionObjArray[index].choices[3]; 
+
+    // once the question and answers are displayed we will listen for a button click on one of the answers 
+
+    // set up an eventListener on each of the buttons
+
+    // when a button is clicked it will trigger handleAnswerClick()
 
 };
 
 var handleAnswerClick = function(event){
-    var buttonText = event.target.innerText;
-    var answer = questionObjArray[index].answer;
-    // check answers here 
-    if(buttonText===answer) {
-        score += 10
-        alert("you go it right!");
+
+    //after the button is clicked we need to look at the text string in the clicked button and we need to look at the answer property string in the questionObjArray and compare them
+
+    // we can use the event.target property to itentify which button was clicked to trigger the event. We can then use the .textContent property to target the textContent on the button that was clicked and store it's value in a variable 
+
+    // we can then target the answer property string from the questionObjArray at the current index and store it's value in a variable 
+
+    var buttonText = event.target.textContent;
+    var answerText = questionObjArray[index].answer;
+
+    // we can check the the answers by comparing the values stored in our buttonText variable and our answerText variable 
+
+    if(buttonText === answerText) {
+        score += 1
+        alert("you go it RIGHT!");
     }
     else {
-        alert("you got it wrong!");
+        alert("Sorry you got it WRONG!");
     }
     
+    // Once we are finished answering the current question we need to move to the next question by incrementing the questionObjArray[index] by +1
     index++
 
-    console.log(index, questionObjArray.length);
+    // if index is greater or = to the length of the questionObjArray length call endQuiz
     if (index >= questionObjArray.length){
         endQuiz();
     }
+    // else continue to question at next index by calling displayQuiz()
     else{
     displayQuiz();
     }
@@ -122,18 +147,27 @@ var handleAnswerClick = function(event){
 
 var endQuiz = function() {
 
-    console.log("this is the end");
-    // when all questions are answered or when array [i] > array.length  or the timer reaches 0 end the game 
-    // end the game by presenting new inner HTML for All Done!
-    // the new inner html with present the user with thier high score and allow them to enter their initials and submit the info to high score page (localStorage)
+    quizContainerEl.classList.add("hide");
 
-    
+    finalScoreFormEl.classList.remove("hide");
+
+    var finalScoreEl = document.getElementById("final-score");
+
+    finalScoreEl.textContent = "Your final score is " + score + "/" + questionObjArray.length + "."
+
+
+
+    // when all questions are answered or when array [i] > array.length  or the timer reaches 0 end the game 
+
 };
 
 
 startButtonEl.addEventListener("click", startQuiz); 
 
 button1.addEventListener("click", handleAnswerClick);
+
 button2.addEventListener("click", handleAnswerClick); 
+
 button3.addEventListener("click", handleAnswerClick); 
+
 button4.addEventListener("click", handleAnswerClick); 
